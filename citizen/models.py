8d311bbd3edbdd6bf9c6e9d2b46d498857698f1f
moses044 <moseswambui044@gmail.com>
2022-11-27 20:40:02 +0300

    First Commit

---
+++ citizen/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Citizen(models.Model):
+    contact = models.CharField(max_length=11)
+    aadhaar  = models.CharField(max_length=12,null=True,blank=True)
+    bhamashah = models.CharField(max_length=12,null=True,blank=True)
+    birth_date = models.DateField(null = True)
+
+    class Meta:
+        verbose_name = 'Citizen'
+
+    def __str__(self):
+        return self.username
+
+
+    def get_absolute_url(self):
+        return reverse("person_detail",kwargs={"id":self.bhamashah})
